5921ffc03f3d31c21d4013934d1715845926c581
Salvatore Pizzolanti <spizzolanti@proton.me>
2025-01-25 16:21:54 +0100

    hopefully ready for deployement

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-        }
+	}
 };
 
 export default config;
